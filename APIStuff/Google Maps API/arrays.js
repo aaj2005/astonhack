edf52637da8cad6c01c01a427d6dda0ebfdf78c3
ltanakwebsite <ltanakbusiness@outlook.com>
2024-02-24 23:07:46 +0000

    URL arrays to use for google maps

---
+++ APIStuff/Google Maps API/arrays.js
@@ -0,0 +1,14 @@
+var urlsAngry = ['https://www.google.com/maps/embed/v1/search?q=parks%20in%20birmingham&key=AIzaSyDTkAgcaDrydO2drr-qO2SSilM55nNTeQ0', 'https://www.google.com/maps/embed/v1/search?q=gyms%20in%20birmingham&key=AIzaSyDTkAgcaDrydO2drr-qO2SSilM55nNTeQ0', 'https://www.google.com/maps/embed/v1/search?q=museums%20in%20birmingham&key=AIzaSyDTkAgcaDrydO2drr-qO2SSilM55nNTeQ0'];
+
+var urlsDisgusted = ['https://www.google.com/maps/embed/v1/search?q=places%20to%20eat%20in%20birmingham&key=AIzaSyDTkAgcaDrydO2drr-qO2SSilM55nNTeQ0', 'https://www.google.com/maps/embed/v1/search?q=spas%20in%20birmingham&key=AIzaSyDTkAgcaDrydO2drr-qO2SSilM55nNTeQ0', 'https://www.google.com/maps/embed/v1/search?q=theatres%20in%20birmingham&key=AIzaSyDTkAgcaDrydO2drr-qO2SSilM55nNTeQ0'];
+
+var urlsFearful = ['https://www.google.com/maps/embed/v1/search?q=religious%20centres%20in%20birmingham&key=AIzaSyDTkAgcaDrydO2drr-qO2SSilM55nNTeQ0', 'https://www.google.com/maps/embed/v1/search?q=coffee%20shops%20in%20birmingham&key=AIzaSyDTkAgcaDrydO2drr-qO2SSilM55nNTeQ0', 'https://www.google.com/maps/embed/v1/search?q=animal%20shelters%20in%20birmingham&key=AIzaSyDTkAgcaDrydO2drr-qO2SSilM55nNTeQ0'];
+
+var urlsHappy = ['https://www.google.com/maps/embed/v1/search?q=shopping%20centres%20in%20birmingham&key=AIzaSyDTkAgcaDrydO2drr-qO2SSilM55nNTeQ0', 'https://www.google.com/maps/embed/v1/search?q=fun%20things%20to%20do%20in%20birmingham&key=AIzaSyDTkAgcaDrydO2drr-qO2SSilM55nNTeQ0', 'https://www.google.com/maps/embed/v1/search?q=entertainment%20in%20birmingham&key=AIzaSyDTkAgcaDrydO2drr-qO2SSilM55nNTeQ0'];
+
+var urlsNeutral = ['https://www.google.com/maps/embed/v1/search?q=restaurants%20in%20birmingham&key=AIzaSyDTkAgcaDrydO2drr-qO2SSilM55nNTeQ0', 'https://www.google.com/maps/embed/v1/search?q=cinema%20in%20birmingham&key=AIzaSyDTkAgcaDrydO2drr-qO2SSilM55nNTeQ0', 'https://www.google.com/maps/embed/v1/search?q=theme%20parks%20in%20birmingham&key=AIzaSyDTkAgcaDrydO2drr-qO2SSilM55nNTeQ0'];
+
+var urlsSad = ['https://www.google.com/maps/embed/v1/search?q=parks%20in%20birmingham&key=AIzaSyDTkAgcaDrydO2drr-qO2SSilM55nNTeQ0', 'https://www.google.com/maps/embed/v1/search?q=artistic%20spaces%20in%20birmingham&key=AIzaSyDTkAgcaDrydO2drr-qO2SSilM55nNTeQ0', 'https://www.google.com/maps/embed/v1/search?q=pubs%20in%20birmingham&key=AIzaSyDTkAgcaDrydO2drr-qO2SSilM55nNTeQ0'];
+
+var urlsSurprised = ['https://www.google.com/maps/embed/v1/search?q=golf%20course%20in%20birmingham&key=AIzaSyDTkAgcaDrydO2drr-qO2SSilM55nNTeQ0', 'https://www.google.com/maps/embed/v1/search?q=hidden%20gems%20in%20birmingham&key=AIzaSyDTkAgcaDrydO2drr-qO2SSilM55nNTeQ0', 'https://www.google.com/maps/embed/v1/search?q=zoos%20in%20birmingham&key=AIzaSyDTkAgcaDrydO2drr-qO2SSilM55nNTeQ0'];
+
