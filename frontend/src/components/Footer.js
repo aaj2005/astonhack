61ede8f0826de7c6a422c143e90042c8d5bdd210
Archii0 <85189932+Archii0@users.noreply.github.com>
2024-02-24 18:57:01 +0000

    DJFHSK
    DSKLFDSLKF

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
   <section class="grey py-4 mt-5 border-top border-light">
     <div class="container overflow-hidden d-flex">
             <a href="#!">
-              <img src="/EmotionSense.png" alt="BootstrapBrain Logo" width="175"/>
+              <img className="me-5" src="/EmotionSense.png" alt="BootstrapBrain Logo" width="175"/>
             </a>
             <p className="ms-5 mt-3 text-white">Your go-to mental health companion</p>
       
