afd203e863342993c6c15fab06f4c33ef3c0e012
Archii0 <85189932+Archii0@users.noreply.github.com>
2024-02-24 13:53:31 +0000

    Camera working

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import CustomWebcam from '../components/CustomWebcam';
 const Home = () => {
 
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://0a9d-134-151-21-91.ngrok-free.app/', {method:"POST"}); 
+        const result = await response.json();
+        console.log(result)
+        setData(result);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      }
+    };
 
+    fetchData();
+  }, []);
+
+  
     return (
-        <></>
+        <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          {/* Render your data here */}
+          <p>{data["1"]}</p>
+        </div>
+      )}
+      <CustomWebcam/>
+    </div>
     )
 }
 
