559edbcd7bf2f2880b485af04e1dbf68a975ffe4
mattm001 <m.macdermott60@gmail.com>
2024-02-24 23:47:32 +0000

    started reccomended websites

---
+++ APIStuff/Website Links/arrays.js
@@ -0,0 +1,14 @@
+var urlsAngry = ['https://www.psychologytoday.com', 'https://www.mindful.org', 'https://www.mindtools.com/'];
+
+var urlsDisgusted = ['https://www.talkspace.com'];
+
+var urlsFearful = ['https://www.betterhelp.com', 'https://www.anxiety.org', ];
+
+var urlsHappy = [''];
+
+var urlsNeutral = [];
+
+var urlsSad = ['https://www.reddit.com/r/mademesmile', 'https://www.7cups.com', ];
+
+var urlsSurprised = ['https://www.bbcearth.com/', ];
+
