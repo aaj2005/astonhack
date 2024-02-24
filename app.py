916f2e05254bdbb2899357d2fd290082bf551bf9
Ali Aljishi <ali.aljishi.2005@gmail.com>
2024-02-24 22:12:56 +0000

    changes i dont remember

--- app.py
+++ app.py
@@ -1,66 +1,65 @@
-from flask import Flask
-# , request
-# from flask_cors import CORS, cross_origin
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
 
-# import cv2
-# import base64
-# import numpy as np
-# from scipy import misc
+import cv2
+import base64
+import numpy as np
+from scipy import misc
 
 #somethin
 app = Flask(__name__)
 
-# CORS(app)
+CORS(app)
 
-# import base64
-# from PIL import Image
-# from io import BytesIO
+import base64
+from PIL import Image
+from io import BytesIO
 
-# def webp_to_jpg(webp_base64_string, output_file_path='output.jpg'):
-#     # Remove the "data:image/webp;base64," prefix
-#     webp_base64_string = webp_base64_string.replace("data:image/webp;base64,", "")
+def webp_to_jpg(webp_base64_string, output_file_path='output.jpg'):
+    # Remove the "data:image/webp;base64," prefix
+    webp_base64_string = webp_base64_string.replace("data:image/webp;base64,", "")
 
-#     # Decode base64 string to binary data
-#     binary_data = base64.b64decode(webp_base64_string)
+    # Decode base64 string to binary data
+    binary_data = base64.b64decode(webp_base64_string)
 
-#     # Open WebP image using Pillow
-#     with Image.open(BytesIO(binary_data)) as webp_image:
-#         # Save as JPG
-#         webp_image.convert('RGB').save(output_file_path, format='JPEG')
+    # Open WebP image using Pillow
+    with Image.open(BytesIO(binary_data)) as webp_image:
+        # Save as JPG
+        webp_image.convert('RGB').save(output_file_path, format='JPEG')
 
 # Example usage
 
 
 @app.route("/", methods=["POST","GET"])
 def index():
-    # if request.method == "GET":
-    return "hello world"
-	# # print(test)
-	# # print(request.headers)
-    # data = request.json["imageData"]
-    # webp_to_jpg(data)
-	# # print(jpg_original)
-	# # img = cv2.imdecode(jpg_as_np, flags=1)
-	# # print(img)
-	# # cv2.imwrite('./images.webp', img)
+    if request.method == "GET":
+        return "hello world"
+	# print(test)
+	# print(request.headers)
+    data = request.json["imageData"]
+    webp_to_jpg(data)
+	# print(jpg_original)
+	# img = cv2.imdecode(jpg_as_np, flags=1)
+	# print(img)
+	# cv2.imwrite('./images.webp', img)
 
-    # return {"1":"hello world"}
+    return {"1":"hello world"}
 
 
-# @app.route("/api/frame", methods=["POST","GET"])
-# def imageProcess():
-#     if request.method == "GET":
-#         return "hello world"
-# 	# print(test)
-# 	# print(request.headers)
-#     data = request.json["imageData"]
-#     webp_to_jpg(data)
-# 	# print(jpg_original)
-# 	# img = cv2.imdecode(jpg_as_np, flags=1)
-# 	# print(img)
-# 	# cv2.imwrite('./images.webp', img)
+@app.route("/api/frame", methods=["POST","GET"])
+def imageProcess():
+    if request.method == "GET":
+        return "hello world"
+	# print(test)
+	# print(request.headers)
+    data = request.json["imageData"]
+    webp_to_jpg(data)
+	# print(jpg_original)
+	# img = cv2.imdecode(jpg_as_np, flags=1)
+	# print(img)
+	# cv2.imwrite('./images.webp', img)
 
-#     return {"1":"hello world"}
+    return {"1":"hello world"}
 
-# if __name__ == "__main__":
-#     app.run(port=2223)
+if __name__ == "__main__":
+    app.run(port=2223)
