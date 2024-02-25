9ede064e58398fc4926a984ad4260297c0d9973c
mattm001 <m.macdermott60@gmail.com>
2024-02-25 01:15:26 +0000

    done lots of work on website+map links

--- APIStuff/Website Links/arrays.js
+++ APIStuff/Website Links/arrays.js
@@ -1,14 +1,14 @@
-var urlsAngry = ['https://www.psychologytoday.com', 'https://www.mindful.org', 'https://www.mindtools.com/'];
+let websitesAngry = ['https://www.psychologytoday.com', 'https://www.mindful.org', 'https://www.mindtools.com/'];
 
-var urlsDisgusted = ['https://www.talkspace.com', 'https://www.happify.com/', 'https://www.nationalgeographic.com/'];
+let websitesDisgusted = ['https://www.talkspace.com', 'https://www.happify.com/', 'https://www.nationalgeographic.com/'];
 
-var urlsFearful = ['https://www.betterhelp.com', 'https://www.anxiety.org', 'https://www.calm.com/'];
+let websitesFearful = ['https://www.betterhelp.com', 'https://www.anxiety.org', 'https://www.calm.com/'];
 
-var urlsHappy = ['https://www.goodnewsnetwork.org/', 'https://www.brainpickings.org/', 'https://www.positive.news/'];
+let websitesHappy = ['https://www.goodnewsnetwork.org/', 'https://www.brainpickings.org/', 'https://www.positive.news/'];
 
-var urlsNeutral = ['https://www.ted.com/talks', 'http://www.duolingo.com', 'https://www.boredpanda.com/'];
+let websitesNeutral = ['https://www.ted.com/talks', 'http://www.duolingo.com', 'https://www.boredpanda.com/'];
 
-var urlsSad = ['https://www.reddit.com/r/mademesmile', 'https://www.7cups.com', ];
+let websitesSad = ['https://www.reddit.com/r/mademesmile', 'https://www.7cups.com', 'https://www.playbuzz.com/'];
 
-var urlsSurprised = ['https://www.bbcearth.com/', 'https://www.atlasobscura.com', ];
+let websitesSurprised = ['https://www.bbcearth.com/', 'https://www.atlasobscura.com', 'https://www.dailygood.org/'];
 
