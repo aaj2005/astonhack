94726e4cc8264b29fd6cf674d849349524838333
Ali Aljishi <ali.aljishi.2005@gmail.com>
2024-02-25 03:57:33 +0000

    changed keras line number

--- AI/SpeedTesting.py
+++ AI/SpeedTesting.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
-from keras.models import model_from_json
 import time
+from keras.models import model_from_json
 
 emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
