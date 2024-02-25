b0bc5c2dd785de3151d8155ddf27d4901b06db55
mattm001 <m.macdermott60@gmail.com>
2024-02-25 00:09:41 +0000

    Recommended websites done

--- APIStuff/Website Links/arrays.js
+++ APIStuff/Website Links/arrays.js
@@ -1,14 +1,14 @@
 var urlsAngry = ['https://www.psychologytoday.com', 'https://www.mindful.org', 'https://www.mindtools.com/'];
 
-var urlsDisgusted = ['https://www.talkspace.com'];
+var urlsDisgusted = ['https://www.talkspace.com', 'https://www.happify.com/', 'https://www.nationalgeographic.com/'];
 
-var urlsFearful = ['https://www.betterhelp.com', 'https://www.anxiety.org', ];
+var urlsFearful = ['https://www.betterhelp.com', 'https://www.anxiety.org', 'https://www.calm.com/'];
 
-var urlsHappy = [''];
+var urlsHappy = ['https://www.goodnewsnetwork.org/', 'https://www.brainpickings.org/', 'https://www.positive.news/'];
 
-var urlsNeutral = [];
+var urlsNeutral = ['https://www.ted.com/talks', 'http://www.duolingo.com', 'https://www.boredpanda.com/'];
 
 var urlsSad = ['https://www.reddit.com/r/mademesmile', 'https://www.7cups.com', ];
 
-var urlsSurprised = ['https://www.bbcearth.com/', ];
+var urlsSurprised = ['https://www.bbcearth.com/', 'https://www.atlasobscura.com', ];
 
